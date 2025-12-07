6a8124f0fe511ef61a5a30af02586be25f43f23c
minaty-mimi <Danielnicholasdibal@gmail.com>
2025-12-07 09:37:38 +0100

    Apply Firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,16 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: User must provide these environment variables
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyBjic-UvhFIvnIXTeGQ3X5TYpZCN2YbHzU",
+    authDomain: "orivon-edge.firebaseapp.com",
+    projectId: "orivon-edge",
+    storageBucket: "orivon-edge.firebasestorage.app",
+    messagingSenderId: "144539070716",
+    appId: "1:144539070716:web:d0dc36f7b49c476b8de779"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
