0c13c2a86795ec48606ec28699eb237cd095a29d
Daniel Nicholas Dibal <Danielnicholasdibal@gmail.com>
2026-08-30 19:25:55 +0100

    fix: remove broken imports and routes from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,6 @@ import StartProject from "./pages/StartProject";
 import BookConsultation from "./pages/BookConsultation";
 import ProjectDetail from "./pages/ProjectDetail";
 import NotFound from "./pages/NotFound";
-import CoreSkillsTrack from "./pages/CoreSkillsTrack";
-import CorporateTrack from "./pages/CorporateTrack";
-import JoinHub from "./pages/JoinHub";
-import PathfindingDashboard from "./pages/PathfindingDashboard";
-import NorthLanding from './pages/NorthLanding';
-import CoursesBrowser from "./pages/CoursesBrowser";
-import LearningPathView from "./pages/LearningPathView";
-import AdminDashboard from "./pages/AdminDashboard";
-import DecklyLanding from './pages/DecklyLanding';
-import DeepRevealLanding from './pages/DeepRevealLanding';
 
 const queryClient = new QueryClient();
 
@@ -41,19 +31,7 @@ const App = () => {
               <Route path="/project/:slug" element={<ProjectDetail />} />
               <Route path="/start-project" element={<StartProject />} />
               <Route path="/book-consultation" element={<BookConsultation />} />
-              <Route path="/core-skills-track" element={<CoreSkillsTrack />} />
-              <Route path="/corporate-track" element={<CorporateTrack />} />
-              <Route path="/join-hub" element={<JoinHub />} />
-              <Route path="/north/dashboard" element={<PathfindingDashboard />} />
-              <Route path="/north/onboarding" element={<NorthLanding />} />
-              <Route path="/north" element={<NorthLanding />} />
-              <Route path="/north/courses" element={<CoursesBrowser />} />
-              <Route path="/north/path" element={<LearningPathView />} />
-              <Route path="/venture/deckly" element={<DecklyLanding />} />
-              <Route path="/pitch-assessor" element={<DecklyLanding />} />
-              <Route path="/north/admin" element={<AdminDashboard />} />
-              <Route path="/venture/deep-reveal" element={<DeepRevealLanding />} />
-              <Route path="/problem-finder" element={<DeepRevealLanding />} />
+
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
